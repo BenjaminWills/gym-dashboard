ce1b6a2154b36fab9b4fd13aa76fe4f779bf7a4f
BenjaminWills <benjaminwills047@gmail.com>
2022-10-27 16:26:45 +0100

    Working sql wrapper

--- sql_wrapper.py
+++ sql_wrapper.py
@@ -1,5 +1,27 @@
 import sqlalchemy as SQLA
+import pandas as pd
 
 class Sql_wrapper:
-    def __init__(self,DB_USER,DB_PASSWORD,DB_HOST,DB_PORT,DB_NAME):
-        pass
+    def __init__(self,username:str,password:str,host:str,port:str,db_name:str):
+        engine = SQLA.create_engine(f"postgresql://{username}:{password}@{host}:{port}/{db_name}")
+        self.engine = engine
+
+    def execute_query(self,query:str) -> pd.DataFrame:
+        """Will execute a query against a selected table.
+
+        Parameters
+        ----------
+        query : str
+            A string 
+
+        Returns
+        -------
+        pd.DataFrame
+            _description_
+        """
+        con = self.engine.connect()
+        results = con.execute(query)
+        returns = results.fetchall()
+        con.close()
+        return pd.DataFrame(returns)
+

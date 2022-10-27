fc8bf36150b79c5525fe7996758e2d11ba7e8f23
BenjaminWills <benjaminwills047@gmail.com>
2022-10-27 17:43:19 +0100

    added docstrings

--- gym_dashboard/utilities.py
+++ gym_dashboard/utilities.py
@@ -4,6 +4,18 @@ from numpy import floor
 
 
 def get_age(DOB:str = "07/03/2001") -> int:
+    """Will return your current age given your DOB
+
+    Parameters
+    ----------
+    DOB : str, optional
+        Date of birth in the format dd/mm/yyyy, by default "07/03/2001"
+
+    Returns
+    -------
+    int
+        Current age as an integer
+    """
     dob_datetime = datetime.datetime.strptime(DOB,'%d/%m/%Y')
     current_date = datetime.datetime.now()
     difference =  current_date - dob_datetime

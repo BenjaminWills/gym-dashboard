b00e7bee28a3cb622938c9333c42601c5d21601c
Benjaminwills <benjaminwills047@gmail.com>
2023-02-25 22:14:58 +0000

    add stylised button

--- gym_gui/gui.py
+++ gym_gui/gui.py
@@ -6,7 +6,7 @@ root = Tk()  # create the base container
 
 # Canvas
 canvas = Canvas(root, width=600, height=300)
-canvas.grid(columnspan=3)  # Can make a grid
+canvas.grid(columnspan=3, rowspan=3)  # Can make a grid
 
 # Logo
 logo = Image.open("gym_gui/images/barbell.png")
@@ -18,5 +18,23 @@ logo_label.grid(column=1, row=0)
 # Instructions
 instructions = Label(root, text="Press the button below!", font="Raleway")
 instructions.grid(columnspan=3, column=0, row=1)
+
+# Browse button
+browse_text = StringVar()
+browse_btn = Button(
+    root,
+    textvariable=browse_text,
+    font="Raleway",
+    bg="#808080",
+    fg="black",
+    height=2,
+    width=15,
+)
+browse_text.set("Browse")
+browse_btn.grid(column=1, row=2)
+
+# Add additional depth
+canvas = Canvas(root, width=600, height=250)
+canvas.grid(columnspan=3)
 
 root.mainloop()

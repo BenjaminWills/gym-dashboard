615097552d3e25299feccea313a131279c3bb731
Benjaminwills <benjaminwills047@gmail.com>
2023-02-25 21:46:51 +0000

    add label and button

--- gym_gui/gui.py
+++ gym_gui/gui.py
@@ -3,4 +3,9 @@ from tkinter import *
 root = Tk() # create the base container
 
 label = Label(root,text="Hello World!") # create a label widget
+button = Button(root,text="Click me!",width=25,height=25,bg="blue",fg="yellow")
 
+button.pack()
+label.pack() # places onto screen
+
+root.mainloop()

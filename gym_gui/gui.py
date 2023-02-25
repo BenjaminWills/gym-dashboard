ee19a7af88c29fa560dd72cec27c3bd0e835af95
Benjaminwills <benjaminwills047@gmail.com>
2023-02-25 22:09:19 +0000

    add gui things

--- gym_gui/gui.py
+++ gym_gui/gui.py
@@ -1,11 +1,22 @@
 from tkinter import *
 
+from PIL import Image, ImageTk
+
 root = Tk()  # create the base container
 
-label = Label(root, text="Hello World!")  # create a label widget
-button = Button(root, text="Click me!", width=25, height=25, bg="blue", fg="yellow")
+# Canvas
+canvas = Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3)  # Can make a grid
 
-button.pack()
-label.pack()  # places onto screen
+# Logo
+logo = Image.open("gym_gui/images/barbell.png")
+logo = ImageTk.PhotoImage(logo)
+logo_label = Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
+
+# Instructions
+instructions = Label(root, text="Press the button below!", font="Raleway")
+instructions.grid(columnspan=3, column=0, row=1)
 
 root.mainloop()

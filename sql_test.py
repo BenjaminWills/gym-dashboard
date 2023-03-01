a5c83c8e327aab3b83852ddbd20f10d6081815be
Benjaminwills <benjaminwills047@gmail.com>
2023-03-01 14:56:33 +0000

    update sql test

---
+++ sql_test.py
@@ -0,0 +1,13 @@
+from utilities.sql.sql_wrapper import Sql_wrapper
+
+
+sql = Sql_wrapper(
+    username="postgres",
+    password="postgres",
+    host="localhost",
+    port=5432,
+    db_name="gym_tracker",
+)
+
+if __name__ == "__main__":
+    print(sql.execute_query("SELECT * FROM test"))

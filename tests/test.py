926d93838842fb5ca33a97134a85774295fd22c4
Benjaminwills <benjaminwills047@gmail.com>
2023-03-01 15:31:20 +0000

    get utils

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+from get_utilities import utilities
+
+from utilities.sql.sql_wrapper import Sql_wrapper
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--username")
+parser.add_argument("--password")
+parser.add_argument("--host")
+parser.add_argument("--port")
+parser.add_argument("--db_name")
+
+args = parser.parse_args()
+
+
+sql = Sql_wrapper(
+    username=args.username,
+    password=args.password,
+    host=args.host,
+    port=args.port,
+    db_name=args.db_name,
+)

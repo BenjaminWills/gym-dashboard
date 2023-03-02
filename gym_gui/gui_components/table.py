c951ff266a5ff6ec0e90909c1a8798dcf0b1fdad
Benjaminwills <benjaminwills047@gmail.com>
2023-03-02 18:19:22 +0000

    add table creater for query results

---
+++ gym_gui/gui_components/table.py
@@ -0,0 +1,26 @@
+from tkinter import Toplevel, Tk, Entry, END
+
+from gui_components.validation.validation import Validate_input
+
+from typing import Tuple
+
+v = Validate_input()
+
+
+class Query_table(Toplevel):
+    def __init__(self, master_root: Tk, query: str) -> None:
+        super().__init__(master=master_root)
+        self.title("create account")
+
+        self.query_result = v.sql_client.execute_read(query)
+
+        self.total_columns = len(self.query_result[0])
+        self.total_rows = len(self.query_result)
+
+        for column in range(self.total_columns):
+            for row in range(self.total_rows):
+                self.e = Entry(
+                    master_root, width=20, fg="blue", font=("Arial", 16, "bold")
+                )
+                self.e.grid(row=row, column=column)
+                self.e.insert(END, self.query_result[row][column])

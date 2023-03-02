1012f08049330bc749d122257deceae35db58a5f
Benjaminwills <benjaminwills047@gmail.com>
2023-03-02 16:53:39 +0000

    update local test

--- sql_test.py
+++ sql_test.py
@@ -14,32 +14,23 @@ table_creation_query = """
         users(
             id SERIAL PRIMARY KEY,
             username VARCHAR(20),
-            password VARCHAR(20)
+            password VARCHAR(20),
+            email VARCHAR(20),
+            height FLOAT,
+            dob DATE
         );"""
 
 table_insertion_query = """
 INSERT INTO 
-    users (username,password)
+    users (username,password,email,height,dob)
 VALUES
-    ('test_user','test_password')
+    ('test_user','test_password','email@email.com',181,'07-03-2001'::date)
 """
 t = "test_user"
 p = "test_password"
 if __name__ == "__main__":
+    # sql.execute_create("DELETE FROM users")
+    # sql.execute_create("DROP TABLE users")
     # sql.execute_create(table_creation_query)
     # sql.execute_create(table_insertion_query)
     print(sql.execute_read("SELECT * FROM users"))
-    print(
-        sql.execute_read(
-            f"""
-    SELECT
-        *
-    FROM
-        users
-    WHERE
-        username = '{t}' 
-        AND 
-        password = '{p}'
-    """
-        )
-    )

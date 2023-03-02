8660dfdfe1a1b90b813f7049b090b0aa4cae50cd
Benjaminwills <benjaminwills047@gmail.com>
2023-03-02 15:23:34 +0000

    add input validation

--- gym_gui/gui_components/create_account.py
+++ gym_gui/gui_components/create_account.py
@@ -1,5 +1,9 @@
-from tkinter import Toplevel, Tk, Canvas, Label
+from tkinter import Toplevel, Tk, Canvas, Label, Text
 from tkcalendar import DateEntry
+
+from gui_components.validation.validation import Validate_input
+
+validation = Validate_input()
 
 
 class Create_account(Toplevel):
@@ -10,12 +14,65 @@ class Create_account(Toplevel):
 
         # Create a canvas
         canvas = Canvas(self, width=300, height=300)
-        canvas.grid(columnspan=3, rowspan=3)
+        canvas.grid(columnspan=3, rowspan=8)
 
+        row_count = 0
         # Insert title label
         title = Label(self, text="Create an account:")
-        title.grid(row=0, column=0, columnspan=3)
+        title.grid(row=row_count, column=0, columnspan=3)
+
+        row_count += 1
+        # Insert username label
+        username_label = Label(self, text="Username:")
+        username_label.grid(row=row_count, column=0)
+
+        # Insert username textbox
+        username_textbox = Text(self, height=1, width=20)
+        username_textbox.grid(row=row_count, column=1)
+
+        row_count += 1
+        # Insert password label
+        password_label = Label(self, text="Password:")
+        password_label.grid(row=row_count, column=0)
+
+        # Insert password textbox
+        password_textbox = Text(self, height=1, width=20)
+        password_textbox.grid(row=row_count, column=1)
+
+        row_count += 1
+        # Insert email label
+        email_label = Label(self, text="Email:")
+        email_label.grid(row=row_count, column=0)
+
+        # Insert email textbox
+        email_textbox = Text(self, height=1, width=20)
+        email_textbox.grid(row=row_count, column=1)
+
+        row_count += 1
+        # Insert height label
+        email_label = Label(self, text="Email:")
+        email_label.grid(row=row_count, column=0)
+
+        # Insert height textbox
+
+        row_count += 1
+        # Insert DOB label
+        dob = Label(self, text="Date of birth:")
+        dob.grid(row=row_count, column=0)
 
         # insert calendar element
-        cal = DateEntry(self)
-        cal.grid(row=2, column=1)
+        dob_entry = DateEntry(self)
+        dob_entry.grid(row=row_count, column=1)
+
+    def __validate_user(self, username: str) -> int:
+        return validation.available_field("username", username).get(
+            "response_code", 400
+        )
+
+    def __validate_pass(self, password: str) -> int:
+        return validation.available_field("password", password).get(
+            "response_code", 400
+        )
+
+    def __validate_email(self, email: str) -> int:
+        return validation.available_field("email", email).get("response_code", 400)

f11d2172bb4d17be1c7c31f369a169f22eaac381
Benjaminwills <benjaminwills047@gmail.com>
2023-03-02 16:53:51 +0000

    add database insertion

--- gym_gui/gui_components/create_account.py
+++ gym_gui/gui_components/create_account.py
@@ -1,5 +1,7 @@
-from tkinter import Toplevel, Tk, Canvas, Label, Text
+from datetime import date
+from tkinter import Toplevel, Tk, Canvas, Label, Text, Button
 from tkcalendar import DateEntry
+
 
 from gui_components.validation.validation import Validate_input
 from get_utilities import utilities
@@ -17,9 +19,9 @@ class Create_account(Toplevel):
         self.title("create account")
 
         # Make logger
-        mkdir_if_not_exists("./log")
+        mkdir_if_not_exists("./logs")
         self.logger = make_logger(
-            logging_path="./log/gui.log", logger_name="create-account"
+            logging_path="./logs/gui.log", logger_name="create-account"
         )
 
         # Create a canvas
@@ -38,6 +40,7 @@ class Create_account(Toplevel):
 
         # Insert username textbox
         username_textbox = Text(self, height=1, width=20)
+        self.username = username_textbox
         username_textbox.grid(row=row_count, column=1)
 
         row_count += 1
@@ -47,6 +50,7 @@ class Create_account(Toplevel):
 
         # Insert password textbox
         password_textbox = Text(self, height=1, width=20)
+        self.password = password_textbox
         password_textbox.grid(row=row_count, column=1)
 
         row_count += 1
@@ -56,6 +60,7 @@ class Create_account(Toplevel):
 
         # Insert email textbox
         email_textbox = Text(self, height=1, width=20)
+        self.email = email_textbox
         email_textbox.grid(row=row_count, column=1)
 
         row_count += 1
@@ -65,6 +70,7 @@ class Create_account(Toplevel):
 
         # Insert height textbox
         height_textbox = Text(self, height=1, width=20)
+        self.height = height_textbox
         height_textbox.grid(row=row_count, column=1)
 
         row_count += 1
@@ -74,7 +80,15 @@ class Create_account(Toplevel):
 
         # insert calendar element
         dob_entry = DateEntry(self)
+        self.dob = dob_entry
         dob_entry.grid(row=row_count, column=1)
+
+        row_count += 1
+        # Insert submit button
+        create_account_button = Button(
+            self, text="create account", command=self.create_user
+        )
+        create_account_button.grid(row=row_count, column=1)
 
     def __validate_user(self, username: str) -> int:
         return validation.available_field("username", username).get(
@@ -89,10 +103,28 @@ class Create_account(Toplevel):
     def __validate_email(self, email: str) -> int:
         return validation.available_field("email", email).get("response_code", 400)
 
+    def __insert_user(
+        self,
+        username: str,
+        password: str,
+        email: str,
+        height: float,
+        dob: date,
+    ):
+        insert_statement = f"""
+        INSERT INTO 
+            users (username,password,email,height,dob)
+        VALUES
+            ('{username}','{password}','{email}',{height},{dob})
+        """
+        validation.sql_client.execute_create(insert_statement)
+
     def create_user(self):
-        username = ""
-        password = ""
-        email = ""
+        username = self.username.get(1.0, "end-1c")
+        password = self.password.get(1.0, "end-1c")
+        email = self.email.get(1.0, "end-1c")
+        height = self.height.get(1.0, "end-1c")
+        dob = self.dob.get_date()
         if (
             self.__validate_email(email) == 200
             and self.__validate_pass(password) == 200
@@ -104,6 +136,11 @@ class Create_account(Toplevel):
                 username: {username}
                 password: {password}
                 email: {email}
+                height:{height}
+                date of birth:{dob}
             """
             )
+            self.__insert_user(username, password, email, height, dob)
+            # Close window on successful register
+            self.destroy()
             pass

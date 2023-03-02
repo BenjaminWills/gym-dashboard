19c6b6e3df84cc9502baf3a5e5e74d0d6e3b6d27
Benjaminwills <benjaminwills047@gmail.com>
2023-03-02 12:44:17 +0000

    add date entry to create account

--- gym_gui/gui_components/create_account.py
+++ gym_gui/gui_components/create_account.py
@@ -1,4 +1,5 @@
-from tkinter import Toplevel, Tk
+from tkinter import Toplevel, Tk, Canvas, Label
+from tkcalendar import DateEntry
 
 
 class Create_account(Toplevel):
@@ -6,3 +7,15 @@ class Create_account(Toplevel):
 
         super().__init__(master=master_root)
         self.title("create account")
+
+        # Create a canvas
+        canvas = Canvas(self, width=300, height=300)
+        canvas.grid(columnspan=3, rowspan=3)
+
+        # Insert title label
+        title = Label(self, text="Create an account:")
+        title.grid(row=0, column=0, columnspan=3)
+
+        # insert calendar element
+        cal = DateEntry(self)
+        cal.grid(row=2, column=1)

401154379cd330cf93b46b5ef35e36212b1bff55
Benjaminwills <benjaminwills047@gmail.com>
2023-03-02 15:23:28 +0000

    change main file

--- gym_gui/gui.py
+++ gym_gui/gui.py
@@ -1,10 +1,11 @@
 from tkinter import Tk
 
 from gui_components.home_window import Home_window
+from gui_components.login import Login_window
 
 root = Tk()  # create the base container
 root.geometry("+%d+%d" % (350, 40))  # Window placement
 
-Home_window(root)
+Login_window(root)
 
 root.mainloop()

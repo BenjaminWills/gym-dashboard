e8fd7d62c407d8f0dded513741b21db571383ed5
Benjaminwills <benjaminwills047@gmail.com>
2023-03-03 10:33:12 +0000

    add treeview table

--- gym_gui/gui_components/table.py
+++ gym_gui/gui_components/table.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel, Tk, Entry, END
+from tkinter import Toplevel, Tk, ttk, Scrollbar, Canvas
 
 from gui_components.validation.validation import Validate_input
 
@@ -8,15 +8,28 @@ v = Validate_input()
 class Query_table(Toplevel):
     def __init__(self, master_root: Tk, query: str) -> None:
         super().__init__(master=master_root)
-        self.title("create account")
+        self.title("Query result")
 
+        # Set geometry of window
+        self.geometry("400x250")
+
+        # Return query result
         self.query_result = v.sql_client.execute_read(query)
+        self.columns = self.query_result[0]
+        self.data = self.query_result[1:]
 
-        self.total_columns = len(self.query_result[0])
-        self.total_rows = len(self.query_result)
+        # Rows and columns of query result
+        rows = len(self.query_result)
+        columns = len(self.query_result[0])
 
-        for column in range(self.total_columns):
-            for row in range(self.total_rows):
-                self.e = Entry(self, width=20, fg="blue", font=("Arial", 16, "bold"))
-                self.e.grid(row=row, column=column)
-                self.e.insert(END, self.query_result[row][column])
+        # Initialise the tree
+        tree_table = ttk.Treeview(self, selectmode="browse")
+        tree_table.grid(row=4, column=1, columnspan=4, padx=5, pady=20)
+
+        tree_table["height"] = 4
+        tree_table["columns"] = self.columns
+        tree_table["show"] = "headings"
+
+        for column in self.columns:
+            tree_table.column(column, anchor="c", width=70)
+            tree_table.heading(column, text=column)

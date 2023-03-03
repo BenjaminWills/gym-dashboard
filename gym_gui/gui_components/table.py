848d03f1d2b5b22bafda29d55d4c628cbbbe28b8
Benjaminwills <benjaminwills047@gmail.com>
2023-03-03 14:43:40 +0000

    add some comments

--- gym_gui/gui_components/table.py
+++ gym_gui/gui_components/table.py
@@ -1,4 +1,7 @@
-from tkinter import Toplevel, Tk, Entry, END
+import csv
+
+from tkinter import Toplevel, Tk, Entry, END, Button, ttk
+from tkinter.filedialog import askopenfile
 
 from gui_components.validation.validation import Validate_input
 
@@ -8,10 +11,14 @@ v = Validate_input()
 class Query_table(Toplevel):
     def __init__(self, master_root: Tk, query: str) -> None:
         super().__init__(master=master_root)
-        self.title("create account")
 
+        # Window title
+        self.title("Query output")
+
+        # Read query
         self.query_result = v.sql_client.execute_read(query)
 
+        # Format and create table
         self.total_columns = len(self.query_result[0])
         self.total_rows = len(self.query_result)
 
@@ -20,3 +27,10 @@ class Query_table(Toplevel):
                 self.e = Entry(self, width=20, fg="blue", font=("Arial", 16, "bold"))
                 self.e.grid(row=row, column=column)
                 self.e.insert(END, self.query_result[row][column])
+
+        # Save output to csv button
+        save_csv_button = Button(text="Save as CSV", command=self.save_output_as_csv)
+        save_csv_button.grid(row=self.total_rows + 1, columnspan=self.total_columns)
+
+    def save_output_as_csv(self):
+        pass
